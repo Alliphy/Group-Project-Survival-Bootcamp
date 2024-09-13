914621d3a8e5eaf796760fd5f357247d16f6622e
Alliphy <ruebaker29@gmail.com>
2024-09-12 18:34:46 -0600

    moved around components within App and Home installed dayjs and flatpickr and added DatePicker component

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,22 +1,35 @@
 import App from "./pages/App.jsx";
+import { Courses } from "./pages/Courses.jsx";
 import { Home } from "./pages/Home.jsx";
-import { Instructors } from "./pages/Instructors.jsx";
 import "./index.css";
 import ReactDOM from "react-dom/client";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="/" element={<Home />} />
-      <Route path="/" element={<Instructors />} />
-    </Route>
-  )
+  [
+    {
+      path: "/",
+      element: <App />,
+      // errorElement: <ErrorPage/>,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/courses",
+          element: <Courses />,
+        },
+      ],
+    },
+  ]
+  // createRoutesFromElements(
+  //   <Route path="/" element={<App />}>
+  //     <Route path="/" element={<Home />} />
+  //     <Route path="/" element={<Instructors />} />
+  //     <Route path="/courses" element={<Courses />} />
+  //   </Route>
+  // )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
